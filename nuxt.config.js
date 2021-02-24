0ab4eb154f7c98f5f25fd120136494e018f6d3c1
Sofiane <pean.sofiane@gmail.com>
2021-02-24 19:01:41 +0100

    ajout pour le deploy test

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,4 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "Feel Good",
     htmlAttrs: {
@@ -13,12 +12,9 @@ export default {
     link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: [],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    { mode: "client", src: "~/plugins/vue-burger-menu" },
+    { mode: "client", src: "~/plugins/vue-burger-menu" }
     // { src: "~/plugins/vuex-persist", mode: "client" }
   ],
 
@@ -35,10 +31,8 @@ export default {
 
   googleFonts: {
     families: {
-      // Nom du font que vous souhaitez utiliser
       Poppins: true
     },
-    // Optionel, mais FORTEMENT conseillé pour des raisons de performance
     display: "swap"
   },
   optimizedImages: {
@@ -46,9 +40,7 @@ export default {
   },
 
   ngrok: {
-    authtoken: process.env.NGROK_TOKEN,
-    // Optionnel, seulement si vous souhaitez limiter l'accès avec un utilisateur / mot de passe
-    // auth: process.env.NGROK_AUTH
+    authtoken: process.env.NGROK_TOKEN
   },
   fontawesome: {
     icons: {
@@ -57,21 +49,19 @@ export default {
     }
   },
 
-  // Modules: https://go.nuxtjs.dev/config-modules
   modules: ["@nuxtjs/axios", "@nuxt/http"],
   router: {
     middleware: ["mobile"]
   },
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-      "src": "nuxt.config.js",
-      "use": "@nuxtjs/vercel-builder",
-      "config": {}
-  },
+  // build: {
+  //   src: "nuxt.config.js",
+  //   use: "@nuxtjs/vercel-builder",
+  //   config: {}
+  // },
 
   loading: {
     color: "orange",
     height: "8px"
-  },
+  }
 };
